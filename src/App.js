9ef2421748c3c131e3002efbd1c75c8d7a5158b0
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-03-25 18:52:27 +0530

    react router dom library installed and created landingpage and home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
     <div>
+      <h3>Media Player App</h3>
+      <LandingPage/>
+      <Home/>
     </div>
   );
 }
