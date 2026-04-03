ef55cb8c148f511daf162c18c034732166c97b1a
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-04-03 19:40:09 +0530

    formatted header component file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Container, Navbar } from 'react-bootstrap';
-import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Navbar className="bg-info">
-        <Container>
-          <Navbar.Brand>
-            <Link to='/' style={{textDecoration:'none'}}>
-              <CloudUploadOutlinedIcon style={{color:"#000000"}}/>
-              <span className='mx-1 fw-bold' style={{color:"#000000"}}>Media Player</span>
-            </Link>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-  )
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <CloudUploadOutlinedIcon style={{ color: "#000000" }} />
+            <span className="mx-1 fw-bold" style={{ color: "#000000" }}>
+              Media Player
+            </span>
+          </Link>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
